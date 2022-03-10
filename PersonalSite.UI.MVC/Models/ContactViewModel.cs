1780dd5b476474881c2198a76c2457ee125123b2
Matthew Roberts <mroberts8035@outlook.com>
2022-03-09 23:58:38 -0600

    Changing contact form to Razor

---
+++ PersonalSite.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; 
+
+namespace PersonalSite.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+       
+        [Required(ErrorMessage = "*Name is required")] 
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Valid Email is required")] 
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [UIHint("MultilineText")] 
+        public string Message { get; set; }
+      
+    }
+}
