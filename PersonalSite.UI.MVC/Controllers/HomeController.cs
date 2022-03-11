17ee18f12979d2b23d016b1145b54b678e5b5074
Matthew Roberts <mroberts8035@outlook.com>
2022-03-11 13:08:55 -0600

    Worked on Contact Form functionality

--- PersonalSite.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite.UI.MVC/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSite.UI.MVC.Models;
 
 namespace PersonalSite.UI.MVC.Controllers
 {
@@ -45,6 +49,33 @@ namespace PersonalSite.UI.MVC.Controllers
         public ActionResult TCFDetails()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"{cvm.Name} has sent you the following message:<br/>" +
+                $"{cvm.Message} <strong>from the email address:</strong> {cvm.Email}.";
+            MailMessage mm = new MailMessage(
+                ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailTo"].ToString(), cvm.Subject, body);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.StackTrace;
+                throw;
+            }
+            return Json(cvm);
         }
 
     }
